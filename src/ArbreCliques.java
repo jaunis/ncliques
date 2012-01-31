855ce43ee803661575fc45f98910fcf1f3d10a70
jaunis <Jean.Aunis@eleves.ec-nantes.fr>
2012-01-31 15:03:19 +0100

    nouvel algo de recherche en cours de codage; pbl: ConcurrentModificationException

--- src/ArbreCliques.java
+++ src/ArbreCliques.java
@@ -1,3 +1,4 @@
+import java.util.Hashtable;
 import java.util.LinkedList;
 
 
@@ -8,12 +9,46 @@ public class ArbreCliques
 	//liste des fils
 	protected LinkedList<ArbreCliques> listeFils = new LinkedList<>();
 	// liste des pères
-	//protected LinkedList<ArbreCliques> listePeres = new LinkedList<>();
+	protected LinkedList<ArbreCliques> listePeres = new LinkedList<>();
 	//hauteur: mis à jour uniquement pour la racine de l'arbre
 	protected int hauteur = 0;
 	//noeuds de l'arbre avec lesquels le noeud courant n'a pas de lien
-	protected LinkedList<ArbreCliques> listeBloquants = new LinkedList<>();
+	//protected LinkedList<ArbreCliques> listeBloquants = new LinkedList<>();
 	
+	//indique si on est remonté après avoir inséré ce processus
+	protected Processus marque;
+	
+	//lors de la remontée, indique par quel fils on est arrivé
+	protected ArbreCliques filsPrecedent;
+	
+	/**
+	 * @return the filsPrecedent
+	 */
+	public ArbreCliques getFilsPrecedent() {
+		return filsPrecedent;
+	}
+
+	/**
+	 * @param filsPrecedent the filsPrecedent to set
+	 */
+	public void setFilsPrecedent(ArbreCliques filsPrecedent) {
+		this.filsPrecedent = filsPrecedent;
+	}
+
+	/**
+	 * @return the marque
+	 */
+	public Processus getMarque() {
+		return marque;
+	}
+
+	/**
+	 * @param marque the marque to set
+	 */
+	public void setMarque(Processus marque) {
+		this.marque = marque;
+	}
+
 	public ArbreCliques(Processus p) {
 		this.valeur = p;
 	}
@@ -21,60 +56,85 @@ public class ArbreCliques
 	public ArbreCliques()
 	{}
 	
-	/*public ArbreCliques dupliquer()
+	/**
+	 * duplique l'arbre courant (copie récursive des fils, copie <b>uniquement des références</b>
+	 * des pères
+	 * @return une copie de l'arbre courant
+	 */
+	public ArbreCliques dupliquer()
 	{
+		System.out.println("appel de dupliquer");
 		ArbreCliques n = new ArbreCliques(valeur);
+		
+		//la table de hachage permet d'associer les anciens noeuds aux nouveaux (ceux qui
+		//seront contenus dans la copie
 		Hashtable<ArbreCliques, ArbreCliques> table = new Hashtable<>();
 		table.put(this, n);
+		//dupliquer(List, Hashtable) fonctionne par effet de bord
 		if(!estFeuille()) dupliquer(listeFils, table);
 		n.setListePeres(new LinkedList<ArbreCliques>(listePeres));
 		return n;
 	}
 	
-	private void dupliquer(LinkedList<ArbreCliques> niveauActuel,
+	/**
+	 * méthode récursive fonctionnant par effet de bord</br>
+	 * L'arbre est parcouru en largeur d'abord. Le niveau de noeuds courant est contenu dans
+	 * niveauActuel.
+	 * @param niveauActuel: liste d'arbres à dupliquer
+	 * @param table: table de hachage associant les anciens noeuds aux nouveaux
+	 */
+	protected void dupliquer(LinkedList<ArbreCliques> niveauActuel,
 							Hashtable<ArbreCliques, ArbreCliques> table)
 	{
 		LinkedList<ArbreCliques> niveauSuivant = new LinkedList<>();
 		for(ArbreCliques a: niveauActuel)
 		{
 			ArbreCliques f = new ArbreCliques(a.getValeur());
+			//maj de la table avec l'arbre a et sa copie
 			table.put(a, f);
+			//recopie de la liste des pères; la table permet d'éviter d'associer les anciens pères 
+			//à un nouveau noeud
 			for(ArbreCliques p: a.getListePeres())
 			{
 				ArbreCliques p2 = table.get(p);
+				//si le père de a n'est pas dans la table, c'est qu'il n'a pas été dupliqué auparavant
+				//dans ce cas on ne cherche pas à dupliquer les pères récursivement
+				//on se contente d'ajouter le père de a à la liste des pères de sa copie
 				if(p2 == null)
 				{
 					f.getListePeres().add(p);
 					p.getListeFils().add(f);
 				}
+				//sinon on associe la copie (p2) du père de a à f
 				else
 				{
 					f.getListePeres().add(p2);
 					p2.getListeFils().add(f);
 				}
 			}
+			//on crée le niveau suivant de noeuds à dupliquer
 			for(ArbreCliques fils: a.getListeFils())
 			{
 				if(!niveauSuivant.contains(fils)) niveauSuivant.add(f);
 			}
 		}
-		
+		// si le niveau suivant est vide, on a terminé, sinon on continue
 		if(!niveauSuivant.isEmpty()) dupliquer(niveauSuivant, table);
-	}*/
+	}
 
 	/**
 	 * @return the listePeres
 	 */
-//	public LinkedList<ArbreCliques> getListePeres() {
-//		return listePeres;
-//	}
+	public LinkedList<ArbreCliques> getListePeres() {
+		return listePeres;
+	}
 
 	/**
 	 * @param listePeres the listePeres to set
 	 */
-//	public void setListePeres(LinkedList<ArbreCliques> listePeres) {
-//		this.listePeres = listePeres;
-//	}
+	public void setListePeres(LinkedList<ArbreCliques> listePeres) {
+		this.listePeres = listePeres;
+	}
 
 	/**
 	 * @return the hauteur
@@ -116,61 +176,82 @@ public class ArbreCliques
 	/**
 	 * @return the listeBloquants
 	 */
-	public LinkedList<ArbreCliques> getListeBloquants() {
-		return listeBloquants;
-	}
+//	public LinkedList<ArbreCliques> getListeBloquants() {
+//		return listeBloquants;
+//	}
 
 	/**
 	 * @param listeBloquants the listeBloquants to set
 	 */
-	public void setListeBloquants(LinkedList<ArbreCliques> listeBloquants) {
-		this.listeBloquants = listeBloquants;
-	}	
+//	public void setListeBloquants(LinkedList<ArbreCliques> listeBloquants) {
+//		this.listeBloquants = listeBloquants;
+//	}	
 	
 	/**
 	 * fonction récursive appelée par toString();
 	 * on n'affiche pas tout l'arbre, uniquement les cliques "valides"
+	 * à utiliser si on se sert de la liste des "bloquants"
 	 * @param chaine
 	 * @return
 	 */
-	protected String toString(String chaine, LinkedList<ArbreCliques> listeInterdits)
-	{
-		if(listeInterdits.contains(this))
-		{
-			return "";
-		}
-		else
-		{
-			if(estFeuille())
-			{
-				String res = chaine + ", " + valeur + "\n";
-				return res.substring(2);
-			}
-			else
-			{
-				String local;
-				if(valeur == null) local = chaine;
-				else local = chaine + ", " + valeur;
-				String res = "";
-				listeInterdits.addAll(listeBloquants);
-				for(ArbreCliques a: listeFils)
-				{
-					res += a.toString(local, new LinkedList<>(listeInterdits));
-				}
-				return res;
-			}
-		}
-		
-	}
+//	protected String toString(String chaine, LinkedList<ArbreCliques> listeInterdits)
+//	{
+//		if(listeInterdits.contains(this))
+//		{
+//			return "";
+//		}
+//		else
+//		{
+//			if(estFeuille())
+//			{
+//				String res = chaine + ", " + valeur + "\n";
+//				return res.substring(2);
+//			}
+//			else
+//			{
+//				String local;
+//				if(valeur == null) local = chaine;
+//				else local = chaine + ", " + valeur;
+//				String res = "";
+//				listeInterdits.addAll(listeBloquants);
+//				for(ArbreCliques a: listeFils)
+//				{
+//					res += a.toString(local, new LinkedList<>(listeInterdits));
+//				}
+//				return res;
+//			}
+//		}
+//		
+//	}
 	
 	/**
 	 * renvoie la liste des cliques (1 clique par ligne)
 	 */
 	public String toString()
 	{
-		return toString("", new LinkedList<ArbreCliques>());
+		//return toString("", new LinkedList<ArbreCliques>());
+		return toString("");
 	}
 	
+	/**
+	 * fonction récursive appelée par toString() quand on utilise un BDD "normal"
+	 * @param prefixe
+	 * @return
+	 */
+	protected String toString(String prefixe)
+	{
+		if(estFeuille())
+			return prefixe + this.valeur + "\n";
+		else
+		{
+			String retour = "";
+			for(ArbreCliques f: listeFils)
+			{
+				retour += f.toString(prefixe + this.valeur + ", ");
+			}
+			return retour;
+		}
+	}
 	/**
 	 * teste si l'élément est une feuille
 	 * @return
@@ -230,7 +311,9 @@ public class ArbreCliques
 		{
 			for(ArbreCliques a: listeFils)
 			{
-				a.addProcessus(nouveau, hauteur - 1, new LinkedList<ArbreCliques>());
+				//si on utilise la liste des "bloquants", décommanter cette ligne et commenter la suivante
+				//a.addProcessus(nouveau, hauteur - 1, new LinkedList<ArbreCliques>());
+				a.addProcessus(nouveau, hauteur-1);
 			}
 			
 		}
@@ -240,46 +323,148 @@ public class ArbreCliques
 	/**
 	 * fonction récursive ajoutant un processus dans l'arbre à une profondeur donnée,<br/>
 	 * en vérifiant que le processus possède une association avec chaque noeud de la branche
+	 * à utiliser quand on a recours à la liste des "bloquants"
 	 * @param p
 	 * @param profondeur
 	 */
-	protected void addProcessus(ArbreCliques nouveau, 
-								int profondeur,
-								LinkedList<ArbreCliques> listeInterdits
-								)
-	{
-		if(valeur.accepte(nouveau.getValeur()))
-		{
-			
-			if(!listeInterdits.contains(this))
-			{
-				if(profondeur == 0)
-				{
-					if(!listeFils.contains(nouveau)) 
-						listeFils.add(nouveau);
+//	protected void addProcessus(ArbreCliques nouveau, 
+//								int profondeur,
+//								LinkedList<ArbreCliques> listeInterdits
+//								)
+//	{
+//		if(valeur.accepte(nouveau.getValeur()))
+//		{
+//			
+//			if(!listeInterdits.contains(this))
+//			{
+//				if(profondeur == 0)
+//				{
+//					if(!listeFils.contains(nouveau)) 
+//						listeFils.add(nouveau);
 //					if(!nouveau.getListePeres().contains(this)) 
 //						nouveau.getListePeres().add(this);
-				}
-				else
+//				}
+//				else
+//				{
+//					listeInterdits.addAll(listeBloquants);
+//					for(ArbreCliques a: listeFils)
+//					{
+//						a.addProcessus(nouveau, profondeur - 1, new LinkedList<>(listeInterdits));
+//					}
+//				}
+//			}
+//			
+//			
+//		}
+//		else
+//		{
+//			listeBloquants.add(nouveau);
+//		}
+//	}
+
+	protected void addProcessus(ArbreCliques nouveau, int profondeur)
+	{
+		if(valeur.accepte(nouveau.getValeur()) && marque != nouveau.getValeur())
+		{
+			if(profondeur == 0)
+			{
+				//2 vérifications valent mieux qu'une; la structure étant complexe, on risque
+				//d'avoir des problèmes inattendus
+				if(!listeFils.contains(nouveau))
 				{
-					listeInterdits.addAll(listeBloquants);
-					for(ArbreCliques a: listeFils)
+					listeFils.add(nouveau);
+					if(!nouveau.listePeres.contains(this))
+						nouveau.listePeres.add(this);
+					for(ArbreCliques pere: listePeres)
 					{
-						a.addProcessus(nouveau, profondeur - 1, new LinkedList<>(listeInterdits));
+						System.out.println("addProcessus:" + this.hashCode() + ", "+ pere.hashCode());
+						pere.remonter(nouveau.getValeur(), this);
 					}
 				}
 			}
-			
-			
+			else
+			{
+				for(ArbreCliques fils: listeFils)
+				{
+					fils.addProcessus(nouveau, profondeur-1);
+				}
+			}
 		}
-		else
+			
+	}
+	/**
+	 * @param nouveau
+	 */
+	protected void remonter(Processus nouveau, ArbreCliques filsPrecedent) 
+	{
+		if(valeur != null)
 		{
-			listeBloquants.add(nouveau);
+			this.filsPrecedent = filsPrecedent;
+			this.marque = nouveau;
+			if(this.valeur.accepte(nouveau))
+			{
+				for(ArbreCliques pere: listePeres)
+				{
+					System.out.println("remonter:" + this.hashCode() + ", " + pere.hashCode());
+					pere.remonter(nouveau, this);
+				}
+			}
+			else
+			{
+				rechercherCroisement(nouveau);
+			}
 		}
 	}
 
 	/**
-	 * supprime les branches de profondeur inf�rieure � la valeur pass�e en param�tre
+	 * @param nouveau
+	 */
+	protected void rechercherCroisement(Processus nouveau) 
+	{
+		if(filsPrecedent.listePeres.size() > 1)
+		{
+			ArbreCliques copie = filsPrecedent.dupliquer();
+			copie.remove(nouveau);
+			//on "casse" le lien avec le fils d'origine
+			filsPrecedent.listePeres.remove(this);
+			listeFils.remove(filsPrecedent);
+			
+			//on crée le lien avec la copie
+			copie.listePeres = new LinkedList<ArbreCliques>();
+			copie.listePeres.add(this);
+			listeFils.add(copie);
+		}
+		else
+			filsPrecedent.rechercherCroisement(nouveau);
+	}
+
+	/**
+	 * supprime les noeuds de l'arbre dont la valeur vaut nouveau
+	 * @param nouveau
+	 */
+	protected void remove(Processus nouveau) 
+	{
+		if(!estFeuille())
+		{
+			ArbreCliques[] listeSuppression = new ArbreCliques[listeFils.size()];
+			int i = 0;
+			for(ArbreCliques fils: listeFils)
+			{
+				if(fils.valeur == nouveau)
+				{
+					listeSuppression[i] = fils;
+					i++;
+				}
+			}
+			for(ArbreCliques a: listeSuppression)
+			{
+				listeFils.remove(a);
+			}
+		}
+	}
+
+	/**
+	 * supprime les branches de profondeur inférieure à la valeur passée en param�tre
 	 * @param profondeur
 	 */
 	protected void nettoyer(int profondeur)
